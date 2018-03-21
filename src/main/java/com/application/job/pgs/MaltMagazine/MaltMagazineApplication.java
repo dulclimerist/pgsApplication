a700296e8c4fb93e7e104782ad442dc41743b040
Andrzej <andrzej.baran91@gmail.com>
2018-03-21 11:44:50 +0100

    Initial commit simple working web app

---
+++ src/main/java/com/application/job/pgs/MaltMagazine/MaltMagazineApplication.java
@@ -0,0 +1,12 @@
+package com.application.job.pgs.MaltMagazine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaltMagazineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaltMagazineApplication.class, args);
+	}
+}
